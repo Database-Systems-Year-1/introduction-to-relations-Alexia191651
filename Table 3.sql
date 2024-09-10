c90cd1d6298b7eeff3fb4ffa20edb086223a2241
Alexia191651 <167401453+Alexia191651@users.noreply.github.com>
2024-09-10 15:50:07 +0300

    Add files via upload

---
+++ Table 3.sql
@@ -0,0 +1,19 @@
+
+
+ CREATE TABLE wrestlers(
+  name VARCHAR(221),
+  age INT,
+  year INT,
+  primary key (name)
+  );
+
+  INSERT INTO wrestlers(name,age,year)
+  VALUES ('Roman Reigns',39,2024);
+
+  Select * from wrestlers;
+
+  
+
+
+
+ 
