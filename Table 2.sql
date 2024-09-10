30b2661cc74c5671cf543e465309931c2c7d9a6f
Alexia191651 <167401453+Alexia191651@users.noreply.github.com>
2024-09-10 15:40:41 +0300

    Add files via upload

---
+++ Table 2.sql
@@ -0,0 +1,18 @@
+
+
+ CREATE TABLE students(
+  name VARCHAR(221),
+  id INT,
+  age INT,
+  year INT,
+  primary key (id)
+  );
+
+  INSERT INTO students (name,id,age,year)
+  VALUES ('Kamunya',183508,19,1);
+
+  Select * from students;
+
+
+
+ 
