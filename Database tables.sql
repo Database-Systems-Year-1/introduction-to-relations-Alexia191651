259e7d01581a659ba22f73909c77be0315ae3572
Alexia191651 <167401453+Alexia191651@users.noreply.github.com>
2024-09-10 12:11:12 +0300

    Add files via upload

---
+++ Database tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cars(
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  year INT,
+  primary key (brand)
+  );
+
+  INSERT INTO cars (brand,model,year)
+  VALUES ('Ford','Mustang',1964);
+
+  Select * from cars;
+ 
